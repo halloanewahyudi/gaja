adac092c2d7398e00637d8fc2116411e55a3cb6b
halloanewahyudi <30697981+halloanewahyudi@users.noreply.github.com>
2025-01-06 14:00:42 +0700

    gray

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
         </div>
       </div>
     </section>
-    <section class="bg-neutral-50 py-20">
+    <section class="bg-neutral-100 py-20">
       <div class="container">
         <div class="grid grid-cols-1 md:grid-cols-2 justify-center gap-6 lg:gap-10">
           <div class="pl-8 p-6 border border-l-4  lg:border-l-8 border-neutral-200 border-l-red-400 bg-white" v-for="item in counter" :key="item">
